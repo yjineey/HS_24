94172f135684255ebd784993060109a4039c85d1
yjineey <did3706@naver.com>
2024-10-22 23:14:30 +0900

    vue public 경로 설정

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,24 @@
 const webpack = require('webpack');
 
 module.exports = {
+  publicPath: '/yjineey-vue2',
   configureWebpack: {
     // Set up all the aliases we use in our app.
     plugins: [
       new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 6
-      })
-    ]
+        maxChunks: 6,
+      }),
+    ],
   },
   pwa: {
     name: 'Vue Argon Design',
     themeColor: '#172b4d',
     msTileColor: '#172b4d',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: '#172b4d'
+    appleMobileWebAppStatusBarStyle: '#172b4d',
   },
   css: {
     // Enable CSS source maps.
-    sourceMap: process.env.NODE_ENV !== 'production'
-  }
+    sourceMap: process.env.NODE_ENV !== 'production',
+  },
 };
