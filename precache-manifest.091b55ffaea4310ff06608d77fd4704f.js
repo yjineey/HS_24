1020efc5a08d8ed937f63df733a8b4000732f15c
yjineey <yjineey@users.noreply.github.com>
2024-10-29 16:11:20 +0000

    deploy: 2f2ef994b6b8729cf253c80d3bab46f7b147bc00

--- precache-manifest.091b55ffaea4310ff06608d77fd4704f.js
+++ precache-manifest.091b55ffaea4310ff06608d77fd4704f.js
@@ -28,12 +28,16 @@ self.__precacheManifest = [
     "url": "/HS_24/img/github-color.4b8a4845.svg"
   },
   {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/HS_24/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
     "revision": "2569aaea6eaaf8cd210db7f2fa016743",
     "url": "/HS_24/fonts/nucleo-icons.2569aaea.woff"
   },
   {
-    "revision": "426439788ec5ba772cdf94057f6f4659",
-    "url": "/HS_24/fonts/nucleo-icons.42643978.woff2"
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/HS_24/fonts/nucleo-icons.f82ec6ba.ttf"
   },
   {
     "revision": "c1733565b32b585676302d4233c39da8",
@@ -42,10 +46,6 @@ self.__precacheManifest = [
   {
     "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
     "url": "/HS_24/img/nucleo-icons.0b8a30b1.svg"
-  },
-  {
-    "revision": "f82ec6ba2dc4181db2af35c499462840",
-    "url": "/HS_24/fonts/nucleo-icons.f82ec6ba.ttf"
   },
   {
     "revision": "b08a8caf60cc789199394946a140c737",
@@ -60,20 +60,20 @@ self.__precacheManifest = [
     "url": "/HS_24/img/vue-color.b9b6cf95.svg"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/HS_24/fonts/fontawesome-webfont.af7ae505.woff2"
-  },
-  {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/HS_24/fonts/fontawesome-webfont.674f50d2.eot"
   },
   {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/HS_24/fonts/fontawesome-webfont.b06871f2.ttf"
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/HS_24/fonts/fontawesome-webfont.af7ae505.woff2"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/HS_24/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/HS_24/fonts/fontawesome-webfont.b06871f2.ttf"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
@@ -92,7 +92,7 @@ self.__precacheManifest = [
     "url": "/HS_24/img/profilePicture3.ac89eaea.jpg"
   },
   {
-    "revision": "9183d3ebec5e5b8a62af1d50558f160d",
+    "revision": "e5e6904048847a3ed5d7ccfe60b30883",
     "url": "/HS_24/index.html"
   },
   {
@@ -140,24 +140,24 @@ self.__precacheManifest = [
     "url": "/HS_24/img/brand/creativetim-white-slim.png"
   },
   {
-    "revision": "b9949387c6179e2dc4c675134a7b7935",
-    "url": "/HS_24/img/brand/favicon.png"
-  },
-  {
     "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
     "url": "/HS_24/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/HS_24/img/brand/favicon.png"
   },
   {
     "revision": "6fafe4baca9d50d61a898c84ade7afa3",
     "url": "/HS_24/img/brand/white.png"
   },
   {
-    "revision": "d14c18447203d0072b47be9526127b01",
-    "url": "/HS_24/img/logo/npm-color.svg"
-  },
-  {
     "revision": "594b1ee1d95ada356eaad078e9217932",
     "url": "/HS_24/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "d14c18447203d0072b47be9526127b01",
+    "url": "/HS_24/img/logo/npm-color.svg"
   },
   {
     "revision": "c06d3c068de68e5654d05552e26e3452",
@@ -168,6 +168,10 @@ self.__precacheManifest = [
     "url": "/HS_24/img/logo/vscode-color.svg"
   },
   {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/HS_24/img/theme/img-1-1200x1000.jpg"
+  },
+  {
     "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
     "url": "/HS_24/img/theme/img-2-1200x1000.jpg"
   },
@@ -176,32 +180,28 @@ self.__precacheManifest = [
     "url": "/HS_24/img/theme/profile.jpg"
   },
   {
-    "revision": "6b07281e253836a2733be8f1189cb22b",
-    "url": "/HS_24/img/profilePicture2.jpg"
-  },
-  {
-    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
-    "url": "/HS_24/img/theme/img-1-1200x1000.jpg"
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/HS_24/img/theme/promo-1.png"
   },
   {
     "revision": "dc49ad52655e1d9d0552c026db3ef688",
     "url": "/HS_24/img/theme/landing.jpg"
   },
   {
-    "revision": "974088a1931e40895bac6db119c62448",
-    "url": "/HS_24/img/theme/promo-1.png"
-  },
-  {
     "revision": "edc7106b21ec12e57022b2ebd534cd2d",
     "url": "/HS_24/img/theme/team-1-800x800.jpg"
   },
   {
-    "revision": "be997d5226b992ffad34816870c6b7aa",
-    "url": "/HS_24/img/theme/team-2-800x800.jpg"
-  },
-  {
     "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
     "url": "/HS_24/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "6b07281e253836a2733be8f1189cb22b",
+    "url": "/HS_24/img/profilePicture2.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/HS_24/img/theme/team-2-800x800.jpg"
   },
   {
     "revision": "66618a418175ddf2ac8c47a241d327a8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/HS_24/precache-manifest.84397e9e0241c479a75fcbc39248932a.js"
+  "/HS_24/precache-manifest.091b55ffaea4310ff06608d77fd4704f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "HS_24"});
